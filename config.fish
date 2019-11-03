d71f6fa0b97e62bd950207dde0bd85aed38c924f
Cameron Cairns <cameron@peppersf.com>
2019-11-03 13:21:41 -0800

    Adds config to fzf to use fd
    This is to add functionality like ignoring files found in .gitignore to
    allow the return of my vim plugin's Files command to not show things
    like pyc files

--- config.fish
+++ config.fish
@@ -1,3 +1,9 @@
+# Sets vim config for stuff like less
 set -x VISUAL vim
 set -x EDITOR vim
+
+# Has FZF use ag by default so you get nice stuff like using .gitignore
+set -x FZF_DEFAULT_COMMAND "fd --type f"
+
+# Allows for local binaries to be installed
 set -x PATH /home/cameron/usr/local/bin $PATH
