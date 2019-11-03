55af2232c769ee2eb2fbe4fa1d9b49cc80bd1fd6
Cameron Cairns <cameron@peppersf.com>
2019-11-03 13:22:20 -0800

    Adds configuration for pyenv and pyenv virtualenv

--- config.fish
+++ config.fish
@@ -7,3 +7,15 @@ set -x FZF_DEFAULT_COMMAND "fd --type f"
 
 # Allows for local binaries to be installed
 set -x PATH /home/cameron/usr/local/bin $PATH
+
+# START PyEnv Settings
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+
+if command -v pyenv 1>/dev/null 2>&1
+ pyenv init - | source
+end
+
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
+# END PyEnv
